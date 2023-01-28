f61539899762b865a23d2e086d7a647c7f0d24cf
Boi.zip <boirpm@outlook.com>
2023-01-28 19:32:47 +0100

    looking for the error in production.

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+console.log(path.join(__dirname, '/public'));
 
 app.use(function(req, res, next) {
   res.status(404)
