8159babcb350f15b33dcd70e358bfd424481f907
Boi.zip <boirpm@outlook.com>
2023-01-28 19:06:43 +0100

    Fixing...

--- public/src/components/accordion/accordion.js
+++
@@ -1,8 +0,0 @@
-export function addAccordion() {
-  let acc = document.querySelectorAll(".accordion_header");
-  for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-      this.parentElement.classList.toggle("active");
-    });
-  }
-}
--- public/src/components/gambMenu/gambMenuTemplates.js
+++
@@ -1,6 +0,0 @@
-export function gambMenuTemplate() {
-  let gambMenu = "";
-  let components = document.querySelectorAll("#site > div");
-  let menuItems = "menu";
-  return `<div id='gambMenu'>${menuItems}</div>`;
-}
